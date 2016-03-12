b4e34b6d8c965fa8dab9cd96cbcca368466ed903
Juan Carlos Veizaga Cabrera <veizagacabrerajuancarlos.jcvc@gmail.com>
2016-03-11 21:11:22 -0400

    ultimo

--- controlador_web.py
+++ controlador_web.py
@@ -10,6 +10,35 @@ logger = logging.getLogger( 'monitorps.controlador' )
 webapp = Blueprint('psdash', __name__, static_folder='publico')
 import alertas
 
+import smtplib
+import ssl
+def enviar_alerta( email , mensaje ):
+    import smtplib
+    correo_gmail = "testing.oz.255@gmail.com"
+    contrasenia_gmail = "Aasdf12345"
+    servidor = 'smtp.gmail.com'
+    puerto = 587
+
+    session = smtplib.SMTP( servidor , puerto )
+
+    session.ehlo()
+    session.starttls()
+    session.ehlo
+
+    session.login( correo_gmail , contrasenia_gmail )
+
+    headers = [
+        "From: " + correo_gmail,
+        "Subject: Mensaje de alerta >> TSO",
+        "To: " + email,
+        "MIME-Version: 1.0",
+        "Content-Type: text/html"]
+    headers = "\r\n".join( headers )
+    
+    session.sendmail( correo_gmail , email ,  headers + "\r\n\r\n" + mensaje )
+
+#enviar_alerta( "veizagacabrerajuancarlos.jcvc@gmail.com" , "prueba 0mensaje hola" )
+
 def get_current_node():
     return current_app.psdash.get_node(g.node)
 
@@ -125,7 +154,7 @@ def index():
 
     if data["cpu"].get("system")>2:
        pprint.pprint( data["cpu"].get('user') )
-       alertas.enviar_alerta("veizagacabrerajuancarlos.jcvc@gmail.com", "cpu ecedido" )
+       enviar_alerta("veizagacabrerajuancarlos.jcvc@gmail.com", "cpu exceptcedido" )
 
     return render_template('index.html', **data)
 
